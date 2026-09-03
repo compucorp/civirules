b1b833be7d03136af0734db7c9377311e9ebc60d
shahrukh <shahrukh@compuco.io>
2026-09-03 14:19:32 +0500

    NEUSPRT-568: Improve the overall code

--- managed/Cron_PurgeRuleLog.mgd.php
+++ managed/Cron_PurgeRuleLog.mgd.php
@@ -12,10 +12,10 @@ return [
           'name' => 'Purge CiviRules rule log',
           'description' => 'Deletes CiviRules rule log records older than the configured number of retention days.',
           'run_frequency' => 'Weekly',
-          'api_entity' => 'PurgeCiviRulesRuleLog',
-          'api_action' => 'run',
+          'api_entity' => 'CiviRulesRuleLog',
+          'api_action' => 'purge',
           'parameters' => "retention_days=90\nmax_rows=500000",
-          'is_active' => '1',
+          'is_active' => '0',
         ],
     ],
 ];
